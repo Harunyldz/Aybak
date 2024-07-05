fb07abe0e3c23b8eb3bc5cb222f78b044cd2d19c
Harun Yıldız <67275880+Harunyldz@users.noreply.github.com>
2024-07-05 23:44:01 +0300

    anasayfaya slider yeri ve giriş yazıları eklendi

--- aybak/src/pages/Home/Home.jsx
+++ aybak/src/pages/Home/Home.jsx
@@ -1,9 +1,32 @@
-import "./Home.css"
+import "./Home.css";
+import background from "../../assets/background2.jpg";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-wrapper">
+      <div className="home-slider">
+        <img src={background} alt="" />
+        <div className="slider-text">
+          <span>Her Tanede Kalite, Her Sofrada Lezzet</span>
+        </div>
+      </div>
+      <section className="header">
+        <div className="header-top">
+          <h2>Her Tanede Kalite, Her Sofrada Lezzet</h2>
+          <p>
+            1984 yılından beri tarım sektöründe öncü olan Öden Gıda San. Tic.
+            Ltd. Şti., kaliteyi her zaman en ön planda tutarak sofralarınıza
+            lezzet katıyor. İlk günden bu yana doğallık ve güvenilirlik
+            ilkesinden ödün vermeden üretim yapan firmamız, 1994 yılında Aybak
+            Gıda San. Tic. Ltd. Şti. olarak genişleyen ürün yelpazesiyle
+            pirincin yanı sıra çeşitli bakliyat ürünlerini de sunmaya başladı.
+            Tarladan sofranıza kadar uzanan bu yolculukta, her bir ürünümüzde
+            aynı özen ve kaliteyi bulacaksınız.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
