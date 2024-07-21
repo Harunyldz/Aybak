d17cd7376839f41aef29a18ff798fd08899be71b
Harun Yıldız <67275880+Harunyldz@users.noreply.github.com>
2024-07-21 19:20:45 +0300

    ürünler sayfası başlangıç

--- aybak/src/pages/Urunler/Urunler.jsx
+++ aybak/src/pages/Urunler/Urunler.jsx
@@ -1,7 +1,12 @@
 import "./Urunler.css";
 import { products } from "../../utils/Products";
+import { useState } from "react";
 
 const Urunler = () => {
+  const [selected, setSelected] = useState(products[0].name);
+  const selectedProducts = products.find((item) => item.name === selected);
+
+  console.log(selectedProducts)
   return (
     <div className="urunler-wrapper">
       <div className="urunler-container">
@@ -10,11 +15,21 @@ const Urunler = () => {
           <menu>
             <ul>
               {products.map((urun) => (
-                <li key={urun.id}>{urun.name}</li>
+                <li onClick={()=>setSelected(urun.name)} key={urun.id}>{urun.name}</li>
               ))}
             </ul>
           </menu>
         </header>
+        <main>
+          <section>
+            {selectedProducts.subProducts.map((selectedProduct) => (
+              <figure key={selectedProduct.id}>
+                <img src={selectedProduct.image} alt="" />
+                <figcaption>{selectedProduct.name}</figcaption>
+              </figure>
+            ))}
+          </section>
+        </main>
       </div>
     </div>
   );
