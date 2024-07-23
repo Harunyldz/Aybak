95f336d526592eae9e350181047d4b84e4d26307
Fatih <113294904+fatihoz5758@users.noreply.github.com>
2024-07-24 00:53:23 +0300

    akışı serbest bıraktım

--- aybak/src/pages/FluidSlider/FluidSlider.jsx
+++ aybak/src/pages/FluidSlider/FluidSlider.jsx
@@ -17,7 +17,7 @@ const FluidSlider = () => {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3000 // Otomatik kaydırma hızı (milisaniye cinsinden)
     };
 
