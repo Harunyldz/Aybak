cf3bb240504096bd4526f2308fb354dd7760f574
Harun Yıldız <67275880+Harunyldz@users.noreply.github.com>
2024-08-10 18:45:59 +0300

    mainslider içindeki resimlerde değişiklik yapıldı

--- aybak/src/components/Slides/Ucuncu/Ucuncu.jsx
+++ aybak/src/components/Slides/Ucuncu/Ucuncu.jsx
@@ -1,21 +1,20 @@
-import "./Ucuncu.css"
-import odenay from "../../../assets/odenay.jpg"
-import aybak  from "../../../assets/slider/foto2.jpg"
-import ekran from "../../../assets/ekran.png"
+// import "./Ucuncu.css"
+// import odenay from "../../../assets/odenay.jpg"
+// import aybak  from "../../../assets/slider/foto2.jpg"
 
-const Ucuncu = () => {
-  return (
-    <div className='ucuncu'>
-      <div className="ucuncu-box">
-        <div className="ucuncu-img-box">
-          <img src={odenay} alt="" />
-        </div>
-        <div className="ucuncu-img-box">
-          <img src={aybak} alt="" />
-        </div>
-      </div>
-    </div>
-  )
-}
+// const Ucuncu = () => {
+//   return (
+//     <div className='ucuncu'>
+//       <div className="ucuncu-box">
+//         <div className="ucuncu-img-box">
+//           <img src={odenay} alt="" />
+//         </div>
+//         <div className="ucuncu-img-box">
+//           <img src={aybak} alt="" />
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Ucuncu
+// export default Ucuncu
