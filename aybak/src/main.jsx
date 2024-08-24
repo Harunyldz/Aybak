94d151ca56b78c8c91b525482a43f95cca81aa83
Harun Yıldız <67275880+Harunyldz@users.noreply.github.com>
2024-08-24 17:58:21 +0300

    sayfa yenileme sorunu çözüldü

--- aybak/src/main.jsx
+++ aybak/src/main.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import Modal from "react-modal"; // Modal kütüphanesini import ediyoruz
 
 Modal.setAppElement("#root"); 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
